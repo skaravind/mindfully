401d8c1d52224421e0592267cd3af5fd11a8e458
S K Aravind <aravindraghvi@gmail.com>
2022-10-03 14:38:48 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,11 +3,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 var queue = require('express-queue');
 const routes = require('./routes');
+const cors = require('cors');
 //const { createTunnel } = require('./helpers/tunnel');
 
 const port = 3100;
 const host = "0.0.0.0";
 const app = express();
+app.use(cors());
 app.use(queue({ activeLimit: 1, queuedLimit: -1 }));
 app.use(bodyParser.json());
 app.use(routes);
